21e644d3ee066baefc814f608a6b01bc33132ce4
Eror <eror.ferizi@pulla.digital>
2024-02-20 16:07:39 +0100

    Define Sections

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,14 @@ import { AboutUsComponent } from './components/about-us/about-us.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { FaqComponent } from './components/faq/faq.component';
+import { BrandsComponent } from './components/brands/brands.component';
+import { ModalComponent } from './components/modal/modal.component';
+import { BlogSliderComponent } from './components/blog-slider/blog-slider.component';
+import { BannerComponent } from './components/homepage/banner/banner.component';
+import { ProjectDetailsComponent } from './components/homepage/project-details/project-details.component';
+import { ServicesComponent } from './components/homepage/services/services.component';
+import { AdvertComponent } from './components/homepage/advert/advert.component';
+import { TestimonialComponent } from './components/homepage/testimonial/testimonial.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +26,15 @@ import { FaqComponent } from './components/faq/faq.component';
     AboutUsComponent,
     HeaderComponent,
     FooterComponent,
-    FaqComponent
+    FaqComponent,
+    BrandsComponent,
+    ModalComponent,
+    BlogSliderComponent,
+    BannerComponent,
+    ProjectDetailsComponent,
+    ServicesComponent,
+    AdvertComponent,
+    TestimonialComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/blog-slider/blog-slider.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-slider',
+  templateUrl: './blog-slider.component.html',
+  styleUrls: ['./blog-slider.component.scss']
+})
+export class BlogSliderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
